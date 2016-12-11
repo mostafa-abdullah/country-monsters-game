eec22103568a60259610a3d343df6d9a79c86a38
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-12-11 23:57:14 +0200

    Changed EPS to match new ratios

--- constants.h
+++ constants.h
@@ -3,4 +3,4 @@ enum Direction {Up, Down, Left, Right};
 enum WeaponType {BrickType, SlippersType, MineType};
 const double PI = acos(-1);
 const double GRAVITY = -0.002;
-const double EPS = 0.1;
+const double EPS = 0.5;
